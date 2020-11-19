2fe17ed9bc401a1bfc4ba2da4259594a0b1363cf
Ulysse ARNAUD <mexanga@gmail.com>
2020-11-19 18:52:26 +0100

    MAJ des styles des labels des repositories et ajout d'un label private

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -1,19 +1,24 @@
 import './index.css'
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faCode, faStar, faCodeBranch} from '@fortawesome/free-solid-svg-icons'
+import {faCode, faStar, faCodeBranch, faUserSecret} from '@fortawesome/free-solid-svg-icons'
 import React from "react";
 
 
 function Repository (props) {
 
-    const {name, description, language, stargazers_count, forks, html_url, handleRepositoryClick, fork} = props
+    let {
+        name, description, language, stargazers_count, forks, html_url, handleRepositoryClick, fork, private: _private
+    } = props
 
     return (
         <article className="Repository" data-url={html_url} onClick={handleRepositoryClick} data-is-fork={fork}>
             <div className="Repository-labels">
                 {
                     fork ? <span class="Repository-labels-fork"><FontAwesomeIcon icon={faCodeBranch}/>&nbsp;Fork</span> : null
+                }
+                {
+                    _private ? <span class="Repository-labels-private"><FontAwesomeIcon icon={faUserSecret}/>&nbsp;Private</span> : null
                 }
             </div>
             <span className="Repository-name">{name}</span>
