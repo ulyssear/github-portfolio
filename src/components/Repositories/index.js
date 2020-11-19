e7c6421a43406b5478318939140ff0097f89f7aa
Ulysse ARNAUD <mexanga@gmail.com>
2020-11-19 16:56:25 +0100

    Ajout d'une condition lorsque l'utilisateur n'a pas de repos

--- src/components/Repositories/index.js
+++ src/components/Repositories/index.js
@@ -2,18 +2,20 @@ import './index.css'
 import React from "react";
 import Repository from "../Repository";
 
-function Repositories (props) {
+function Repositories(props) {
 
     let {repositories, handleRepositoryClick} = props
     if ('string' === typeof repositories) repositories = JSON.parse(repositories)
 
-    console.log({repositories})
-
     return (
         <section className="Repositories">
-            {repositories.map(repository => (
-                <Repository key={repository.id} {...repository} handleRepositoryClick={handleRepositoryClick} />
-            ))}
+            {
+                !! repositories ? repositories.map(repository => (
+                    <Repository key={repository.id} {...repository} handleRepositoryClick={handleRepositoryClick}/>
+                )) : (
+                    <article>No repository found</article>
+                )
+            }
         </section>
     )
 
